35cc6bf30825bc342d4dc27bd9926ae13e93f174
hwangy1 <yuna.hwang@hva.nl>
2026-01-15 09:35:27 +0900

    create project

---
+++ src/main/java/com/kanghwang/khholdings/KhHoldingsApplication.java
@@ -0,0 +1,13 @@
+package com.kanghwang.khholdings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhHoldingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhHoldingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanghwang/khholdings/KhHoldingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanghwang.khholdings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhHoldingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
