b5c236881537951c5972b13578db6f06725daa2d
Yuna Hwang <82206168+hyn4008@users.noreply.github.com>
2026-01-17 13:01:39 +0900

    [FIX] OrderState 오타 수정

--- src/main/java/com/kanghwang/khholdings/domain/order/type/OrderState.java
+++ src/main/java/com/kanghwang/khholdings/domain/order/type/OrderState.java
@@ -1,9 +1,9 @@
 package com.kanghwang.khholdings.domain.order.type;
 
 public enum OrderState {
-	NEW,		//신규
-	PARTIAL,	//부분체결
-	COMPLETED, 	//체결완료
+	NEW,		// 신규
+	PARTIAL,	// 부분체결
+	COMPLETED, 	// 체결완료
 	CANCELLED,	// 주문취소
-	EJECTED		//주문실패
+	REJECTED	// 주문실패
 }
