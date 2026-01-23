6929145c2f0a4ee130c90354d42521597143a98a
hwangy1 <yuna.hwang@hva.nl>
2026-01-23 14:58:16 +0900

    [FEAT] 기본 시간을 Asia/Seoul로 설정

--- src/main/java/com/kanghwang/khholdings/KhHoldingsApplication.java
+++ src/main/java/com/kanghwang/khholdings/KhHoldingsApplication.java
@@ -1,12 +1,22 @@
 package com.kanghwang.khholdings;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+
 @EnableScheduling
 @SpringBootApplication
 public class KhHoldingsApplication {
+
+	@PostConstruct
+	public void started() {
+		// 앱 전체의 기본 시간대를 서울로 고정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KhHoldingsApplication.class, args);
