11f4935f446928cadc4fd85d240acd49116a86f3
Minje Kang <kangminje00@gmail.com>
2026-01-30 05:33:14 +0900

    [FIX] unit 자료형 변경, enum제거, mapper.xml 수정

--- src/main/java/com/kanghwang/khholdings/domain/market/dto/CandleDTO.java
+++ src/main/java/com/kanghwang/khholdings/domain/market/dto/CandleDTO.java
@@ -1,5 +1,6 @@
 package com.kanghwang.khholdings.domain.order.dto;
 
+import com.kanghwang.khholdings.domain.market.type.UnitEnum;
 import lombok.*;
 
 import java.io.Serializable;
@@ -14,7 +15,7 @@ import java.time.OffsetDateTime;
 @AllArgsConstructor
 public class CandleDTO implements Serializable {
     private Long tokenId;               // 토큰 고유 번호
-    private int unit;                   // unit (1분봉이면 1)
+    private UnitEnum unit;                   // unit (1분봉이면 1)
     private Long candleTime;            // 캔들 기준 시간, TrainingView 연동을 위해 Long 사용
     private BigDecimal openingPrice;    // 시가
     private BigDecimal highPrice;       // 고가
