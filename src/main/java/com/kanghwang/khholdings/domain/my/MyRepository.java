a9b8e74827b3d4ed15e3969e0e8338749a435327
hwangy1 <yuna.hwang@hva.nl>
2026-04-02 09:47:14 +0900

    [FEAT] 마이페이지 계좌 생성 추가

--- src/main/java/com/kanghwang/khholdings/domain/my/MyRepository.java
+++ src/main/java/com/kanghwang/khholdings/domain/my/MyRepository.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.kanghwang.khholdings.domain.my.dto.HoldingDTO;
 import com.kanghwang.khholdings.domain.my.dto.MyTransactionHistDTO;
+import com.kanghwang.khholdings.domain.my.dto.UserInfoDTO;
 import com.kanghwang.khholdings.domain.my.dto.WalletDTO;
 
 @Mapper
@@ -19,6 +20,15 @@ public interface MyRepository {
 
 	// 거래 내역 조회(필터 조회, 기간 조회, 페이징)
 	List<MyTransactionHistDTO> selectMyTransactionHist(Long walletId, String category, Integer period, Integer offset);
+
+	// 사용자 조회
+	boolean existsByUserId(Long userId);
+
+	// 계정 생성
+	void createUser(UserInfoDTO userInfo);
+
+	// 계좌 생성
+	void createAccount(Long userId);
 
 	// 계좌 연동
 	Long selectAccount(Long userId);
